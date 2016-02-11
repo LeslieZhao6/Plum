0bdf8e1b388813b56eccacef2f5becdc2c04bcb9
Anshuman <anshuman14021@iiitd.ac.in>
2016-02-11 22:53:16 +0530

    Enter/update details of user in fraud-DB.

--- Backend/validateLogin.js
+++ Backend/validateLogin.js
@@ -1,4 +1,5 @@
 var Twitter = require('twitter');
+var fs      = require('fs');
 
 exports.validateLogin = function (token)
 {
@@ -9,9 +10,23 @@ exports.validateLogin = function (token)
   	access_token_secret: 'LosMj2fAEKuzlPMKcvbL1pymuD4BYY1nv8cw3Vmy9AjLs'
 	});
 	client.get('account/verify_credentials',{include_email : true, skip_status : true, include_entities : false},function(error, response) {
-		console.log(response);
+		// console.log(response);
 		if (!error) {
-	      console.log("Validated");
+	      var all_data;
+		  var id = response['screen_name'];
+		  var this_user = {};
+	      this_user[id] = { name: response['name'],location: "Location", gender: "Male" };
+	      // console.log(this_user);
+	      fs.readFile('user_list.json', 'utf8', function (err, data) {
+  		  	if (err) throw err;
+  		  	all_data = JSON.parse(data);
+	 		all_data[id] = this_user[id];
+  		  	// console.log(all_data);
+	     	fs.writeFile("user_list.json", JSON.stringify(all_data), function(err) {
+    			if(err) throw err;
+				});
+			});
+	      console.log("Updated \"DB\"");
 	    }
 	     else {
 	      console.log(error);

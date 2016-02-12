c6dd2c6a52aa2aaeb05fed60ef32a3fc9c1b88bf
Anshuman <anshuman14021@iiitd.ac.in>
2016-02-12 18:16:13 +0530

    Sentiment score

--- Backend/alchemy.js
+++ Backend/alchemy.js
@@ -3,15 +3,26 @@ var watson = require('watson-developer-cloud');
 var alchemy_language = watson.alchemy_language({
   api_key: 'd6aee88a1a9b99038ca060ca08c30848d9ed02a0'
 });
- 
-var params = {
-  text: 'Forgetting, opium of the people, Bandwagoning, acid throwing, sarcifice,Political correctness',
-  maxRetrieve: 5
-};
- 
-alchemy_language.keywords(params, function (err, response) {
-  if (err)
-    console.log('error:', err);
-  else
-    console.log(response['keywords']);
-});
+
+
+exports.getSentiment = function(tweet, cb)
+{
+	var sentiment = 0;
+	var params = {
+  	  text: tweet
+	};
+	alchemy_language.sentiment(params, function (err, response) {
+ 	 if (err)
+    	console.log('error:', err);
+  	else
+  	{
+  		if( 'score' in response['docSentiment'])
+  		{
+  			sentiment = response['docSentiment']['score'];
+  		}	
+  	}
+	    console.log(sentiment);
+	});
+}
+
+exports.getSentiment( 'potatoes are tasty', function(e , r){});
